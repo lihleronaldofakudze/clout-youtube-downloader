f2899d3d1c5ef5c729ef6c5c9a11cd496190b31c
CodeCloutDev <71075659+CodeCloutDevs@users.noreply.github.com>
2022-03-21 19:33:48 +0200

    clout youtube downloader, command line app done

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from pytube import YouTube
 
 link = input('enter your youtube url : ')
-# yt = 
+yt = YouTube(link)
+videos = yt.streams.all()
+
+video = list(enumerate(videos))
+
+for i in video:
+    print(i)
+    
+print('enter the disired option to download the format')
+dn_option = int(input('enter the option : '))
+
+dn_video = videos[dn_option]
+dn_video.download()
+
+print("download successfully")
